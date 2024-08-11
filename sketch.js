2b6156e763e87f6f8b42fb2dbbe43bb79eb0b939
farid raouf <farid@local.com>
2024-08-11 12:53:17 +0300

    Playing around

--- sketch.js
+++ sketch.js
@@ -1,14 +1,25 @@
 function setup() {
-  createCanvas(400, 400);
+  let cnv = createCanvas(400, 400);
+
+  background(220);
+  cnv.position(100, 200);
 }
 
 function draw() {
-  background(220);
-  MakeCircle(200,200,20);
-  MakeCircle(220,200,20);
-}
+  centerx=200
+  centery=200
+  bigd = 200
+  littled = 140
+  fill(255)
+  circle(centerx,centery,bigd)
+  fill(100)
+  circle(centerx,centery,littled) 
+  
+  for (let i = 0 ; i <4; i++){
+    fill(50)
+    circle(centerx + 85*cos(i* PI/2) ,centery + 85*sin(i*PI/2),(bigd-littled)/2)
 
-function MakeCircle(x,y,d) {
-    
-  circle(x,y,d);
+
+  }
+  
 }
