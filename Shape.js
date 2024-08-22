394f6720339e991c85a76f8e971f8f3070d4638a
farid raouf <farid@local.com>
2024-08-22 14:12:42 +0300

    circle following circle

--- Shape.js
+++ Shape.js
@@ -1,27 +1,31 @@
 class Shape {
-  constructor( x_center,   y_center)
+  constructor( x_center,   y_center , r)
   {
       this.x_center = x_center;
       this.y_center = y_center;
+      this.r = r * 2;
   }
 
 
 
   Show() {
-    circle(this.x_center, this.y_center, 40)
+    circle(this.x_center, this.y_center, this.r)
     
   }
 
+  GetUp(){
+    this.y_center -= 1
+  }
   GetDown(){
-    this.y_center += 2
+    this.y_center += 1
   }
 
   MoveLeft(){
-    this.x_center -= 40
+    this.x_center -= 1
   }
 
   MoveRight(){
-    this.x_center += 40
+    this.x_center += 1
   }
   
 }
--- sketch.js
+++ sketch.js
@@ -1,27 +1,45 @@
+
+// shapes = []
+// counter = 0
+
 function setup() {
   let cnv = createCanvas(380, 600);
   background(220)
   cnv.position(20, 100);
   //frameRate(3); // three times each second
-  shapes = []
-  shapes[0] = new Shape(100, 100)
-  counter = 0
+  a = new Shape(200,200, 100)
+  b = new Shape(200,200,40)
 }
 
 function draw() {
   background(220)
-  for (let i = 0; i < shapes.length; i++) {
-    shapes[i].GetDown()
+  a.x_center = mouseX
+  a.y_center = mouseY
+  
+  a.Show()
+  b.Show()
 
-    shapes[i].Show()
+  distance = sqrt((a.x_center - b.x_center)**2 + (a.y_center - b.y_center)**2)
+  while ( distance > 60){
+    a.Show()
+    // background(220)
+    if ( a.x_center-b.x_center > 0){b.MoveRight()}
+    else{b.MoveLeft()}
+
+    if ( a.y_center-b.y_center > 0){b.GetDown()}
+    else{b.GetUp()}
+    
+
+    
+    distance = sqrt((a.x_center - b.x_center)**2 + (a.y_center - b.y_center)**2)
+    
   }
+  b.Show()
 }
+
+
 function mouseClicked() {
-  counter += 1
-  shapes[counter] = new Shape(mouseX, mouseY)
-
-  shapes[counter].Show()
-
+  
 
 
 }
@@ -30,13 +48,11 @@ function mouseClicked() {
 
 function keyPressed() {
   if (keyCode == LEFT_ARROW) {
-    shapes[counter].MoveLeft()
-    background(220)
-    shapes[counter].Show()
+    a.MoveLeft()
   }
   if (keyCode == RIGHT_ARROW) {
-    shapes[counter].MoveRight()
-    background(220)
-    shapes[counter].Show()
+    a.MoveRight()
   }
+  background(220)
+    a.Show()
 } 
