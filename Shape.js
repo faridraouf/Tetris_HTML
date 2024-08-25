b4364a54cdd97f3e168f255ebd2561215df0991d
farid raouf <farid@local.com>
2024-08-25 05:27:59 +0300

    intersect lines with bugs

--- Shape.js
+++ Shape.js
@@ -1,31 +1,77 @@
 class Shape {
-  constructor( x_center,   y_center , r)
-  {
-      this.x_center = x_center;
-      this.y_center = y_center;
-      this.r = r * 2;
-  }
-
-
-
-  Show() {
-    circle(this.x_center, this.y_center, this.r)
+  constructor(x_center, y_center) {
+    this.x_center = x_center;
+    this.y_center = y_center;
+    this.numofv
+    this.x = []
+    this.y = []
+    this.w = 40
+    this.j = 0
     
   }
 
-  GetUp(){
-    this.y_center -= 1
-  }
-  GetDown(){
-    this.y_center += 1
+  Show() {
+    beginShape();
+    for (let i = 0; i < this.numofv; i++) {
+
+      vertex(this.x_center + (this.x[this.j][i] * this.w), this.y_center + (this.y[this.j][i] * this.w));
+      
+    
+      // Stop drawing the shape.
+      
+    }
+    endShape(CLOSE);
+    circle(this.x_center, this.y_center, 10)
   }
 
-  MoveLeft(){
-    this.x_center -= 1
+  GetUp() {
+    this.y_center -= 10
+  }
+  GetDown() {
+    this.y_center += 10
   }
 
-  MoveRight(){
-    this.x_center += 1
+  MoveLeft() {
+    this.x_center -= 10
   }
+
+  MoveRight() {
+    this.x_center += 10
+  }
+
+}
+
+class Mirror_L extends Shape {
+  constructor(x_center, y_center) {
+    super(x_center, y_center);
+    this.numofv = 6;
+
+    this.x = [
+      [-1,0,0,1,1,-1],
+      [-1.5,1.5,1.5,-0.5,-0.5,-1.5],
+      [-1,1,1,0,0,-1],
+      [-1.5,0.5,0.5,1.5,1.5,-1.5]
+    ]
+
+    this.y = [
+      [-1.5,-1.5,0.5,0.5,1.5,1.5],
+      [-1,-1,0,0,1,1],
+      [-1.5,-1.5,1.5,1.5,-0.5,-0.5],
+      [0,0,-1,-1,1,1]
+    ]
+
+    
+
+  }
+
+
+
+  Rotate() {
+    
+    this.j += 1
+    if(this.j == 4){this.j = 0}
+  }
+
   
+
 }
--- sketch.js
+++ sketch.js
@@ -3,43 +3,75 @@
 // counter = 0
 
 function setup() {
-  let cnv = createCanvas(380, 600);
+  let cnv = createCanvas(1000, 600);
   background(220)
   cnv.position(20, 100);
-  //frameRate(3); // three times each second
-  a = new Shape(200,200, 100)
-  b = new Shape(200,200,40)
+  angleMode(DEGREES);
+ // noLoop() // three times each second
+  x1 = floor(random(0,500))
+  x2 = floor(random(0,500))
+
+  y1 = floor(random(0,500))
+  y2 = floor(random(0,500))
+
+
+
 }
 
 function draw() {
   background(220)
-  a.x_center = mouseX
-  a.y_center = mouseY
+
+ 
+
+  u1 = 500
+  u2 = mouseX
+
+  v1 = 400
+  v2 = mouseY
+
+
+  omega = abs((v1-y2) / (x2 - u1))
+  theta = abs((v1 -v2) / (u2 -u1))
+  a = abs((v1 - y1) / (x1 -u1))
+
+  line(u1, v1, u2, v2)
+  line(x1, y1, x2, y2)
+
   
-  a.Show()
-  b.Show()
 
-  distance = sqrt((a.x_center - b.x_center)**2 + (a.y_center - b.y_center)**2)
-  while ( distance > 60){
-    a.Show()
-    // background(220)
-    if ( a.x_center-b.x_center > 0){b.MoveRight()}
-    else{b.MoveLeft()}
+  m_free = (v2 - v1) / (u2 - u1)
+  m_fix = (y2 - y1) / (x2 - x1)
 
-    if ( a.y_center-b.y_center > 0){b.GetDown()}
-    else{b.GetUp()}
-    
+  Au = (m_fix * x1 + v1 - m_free * u1 - y1) / (m_fix - m_free)
 
-    
-    distance = sqrt((a.x_center - b.x_center)**2 + (a.y_center - b.y_center)**2)
-    
+  Bu = m_fix * (Au - x1) + y1
+
+
+
+  dis = sqrt((v2-v1)**2 + (u2-u1)**2)
+
+  min_dis = sqrt((Au - u1)**2 + (Bu - v1)**2)
+  // Au < x2 && Au > x1 && Bu > y1 && Bu < y2 &&
+  
+  // && theta > omega && theta < a
+  
+  if (dis > min_dis&& theta > min(omega,a) && theta < max(omega,a) )  {
+
+    fill(255, 255, 0)
+    circle(Au, Bu, 20)
   }
-  b.Show()
+  else {
+    fill(255)
+    circle(Au, Bu, 20)
+
+  }
+
+
 }
 
 
 function mouseClicked() {
-  
+
 
 
 }
@@ -47,12 +79,30 @@ function mouseClicked() {
 
 
 function keyPressed() {
-  if (keyCode == LEFT_ARROW) {
-    a.MoveLeft()
+
+  switch (keyCode) {
+    case LEFT_ARROW:
+
+      a.MoveLeft()
+      break;
+    case RIGHT_ARROW:
+
+      a.MoveRight()
+      break;
+    case 82:
+
+      a.Rotate()
+      break;
+
+    case DOWN_ARROW:
+
+      a.GetDown()
+      break;
+
+    default:
+
   }
-  if (keyCode == RIGHT_ARROW) {
-    a.MoveRight()
-  }
+
   background(220)
-    a.Show()
+  a.Show()
 } 
