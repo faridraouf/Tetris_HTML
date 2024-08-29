cc55cf04835d744fa3fc755b112a97c9b3abb1f5
farid raouf <farid@local.com>
2024-08-30 00:27:34 +0300

    tetris without shape shape collision

--- Shape.js
+++ Shape.js
@@ -1,76 +1,142 @@
+class Block{
+  constructor(x_pos,y_pos){
+    this.w = 30;
+    this.x_pos = x_pos;
+    this.y_pos = y_pos;
+           
+  }
+  Show(){
+    fill(255,0,0)
+    square(this.x_pos*this.w,this.y_pos*this.w,this.w)
+    fill(255)
+
+  }
+}
+
 class Shape {
   constructor(x_center, y_center) {
-    this.x_center = x_center;
-    this.y_center = y_center;
-    this.numofv
+    this.w = 30
+    this.x_center = x_center ;
+    this.y_center = y_center ;
+    this.no_of_squares
     this.x = []
     this.y = []
-    this.w = 40
-    this.j = 0
+    this.rotation_no = 0
+    this.blocks = []
+    this.no_of_rotations = 4
     
   }
-
+  Update(){
+    for (let i = 0 ; i < this.no_of_squares ; i++){
+      this.blocks[i] = new Block(this.x_center + (this.x[this.rotation_no][i]), this.y_center + (this.y[this.rotation_no][i] ) )
+    }
+  }
   Show() {
-    beginShape();
-    for (let i = 0; i < this.numofv; i++) {
+    
+    for (let i = 0; i < this.no_of_squares; i++) {
 
-      vertex(this.x_center + (this.x[this.j][i] * this.w), this.y_center + (this.y[this.j][i] * this.w));
-      
+      var test = this.blocks[i]
+      test.Show()
     
       // Stop drawing the shape.
       
     }
-    endShape(CLOSE);
-    circle(this.x_center, this.y_center, 10)
+    
+    circle(this.x_center* this.w  + this.w/2, this.y_center* this.w  + this.w/2, 10)
   }
 
-  GetUp() {
-    this.y_center -= 10
+  MoveUp() {
+    this.y_center -= 1
   }
-  GetDown() {
-    this.y_center += 10
+  MoveDown() {
+    this.y_center += 1
+    this.Update()
   }
 
   MoveLeft() {
-    this.x_center -= 10
+    
+      for (const block of this.blocks) {
+        if(block.x_pos == 0){
+          return;
+        }
+      }
+    
+    this.x_center -= 1
+    this.Update()
   }
 
   MoveRight() {
-    this.x_center += 10
+    for (const block of this.blocks) {
+      if(block.x_pos == 9){
+        return;
+      }
+    }
+    this.x_center += 1
+    this.Update()
+  }
+
+  Rotate() {
+    this.rotation_no += 1
+    if(this.rotation_no == this.no_of_rotations){this.rotation_no = 0}
+    this.Update()
+    for (const block of this.blocks) {
+      if(block.x_pos > 9){
+        this.x_center -=  1
+        break;
+      }
+      else if(block.x_pos < 0){
+        this.x_center += 1
+        break;
+      }
+    }
+    
+    this.Update()
+    
+
+    
+  }
+
+  ReachedBottom() {
+    for (const element of this.blocks) {
+      if (element.y_pos == 19){
+        return true ;
+      }
+    }
+    return false;
   }
 
 }
 
-class Mirror_L extends Shape {
+class L extends Shape {
   constructor(x_center, y_center) {
     super(x_center, y_center);
-    this.numofv = 6;
-
+    this.no_of_squares = 4;
+    
     this.x = [
-      [-1,0,0,1,1,-1],
-      [-1.5,1.5,1.5,-0.5,-0.5,-1.5],
-      [-1,1,1,0,0,-1],
-      [-1.5,0.5,0.5,1.5,1.5,-1.5]
+      [0,0,0,1],
+      [-1,-1,0,1],
+      [-1,0,0,0],
+      [-1,0,1,1]
     ]
 
     this.y = [
-      [-1.5,-1.5,0.5,0.5,1.5,1.5],
-      [-1,-1,0,0,1,1],
-      [-1.5,-1.5,1.5,1.5,-0.5,-0.5],
-      [0,0,-1,-1,1,1]
+      [-1,0,1,1],
+      [1,0,0,0],
+      [-1,-1,0,1],
+      [0,0,0,-1]
     ]
 
     
 
+    for (let i = 0 ; i < this.no_of_squares ; i++){
+      this.blocks.push(new Block(this.x_center - (this.x[this.rotation_no][i]), this.y_center - (this.y[this.rotation_no][i] ) ))
+    }
+
   }
 
 
 
-  Rotate() {
-    
-    this.j += 1
-    if(this.j == 4){this.j = 0}
-  }
+  
 
   
 
--- sketch.js
+++ sketch.js
@@ -1,43 +1,98 @@
 
-// shapes = []
-// counter = 0
 
 function setup() {
-  let cnv = createCanvas(1000, 600);
+  let cnv = createCanvas(500, 700);
   background(220)
-  cnv.position(20, 100);
-  angleMode(DEGREES);
- // noLoop() // three times each second
-  x1 = floor(random(0,500))
-  x2 = floor(random(0,500))
+  cnv.position(20, 20);
 
-  y1 = floor(random(0,500))
-  y2 = floor(random(0,500))
+  frameCount;
+  rows = 20
+  cols = 10
+  shapes = []
+  newshape = 1
+  count = -1
+//frameRate(1)
+  row_num = 0
+  
+
+  grid = Array(rows).fill().map(() => Array(cols).fill(0));
+}
+
+function draw() {
+  grid = Array(rows).fill().map(() => Array(cols).fill(0));
+
+  rect(0, 0, 300, 600)
+  for (let i = 0; i < 10; i++) {
+    line((i * 30), 0, (i * 30), 600)
+  }
+if(newshape == 1){
+  shapes.push(new L(2,2))
+  count  += 1
+newshape = 0
+}
+
+  for (const element of shapes) {
+    element.Show()
+    for (const block of element.blocks) {
+      grid[block.y_pos][block.x_pos] = 1
+    }
+  }
+  
+if (frameCount % 20 == 0){
+  
+    //shapes[count].MoveDown()
+}
+  
+if(shapes[count].ReachedBottom()){
+  newshape  = 1
+}
 
 
 
 }
 
-function draw() {
+
+
+
+
+function keyPressed() {
+
+  switch (keyCode) {
+    case LEFT_ARROW:
+
+      shapes[count].MoveLeft()
+      break;
+    case RIGHT_ARROW:
+
+    shapes[count].MoveRight()
+      break;
+    case 82:  // R letter
+
+    shapes[count].Rotate()
+      break;
+
+    case DOWN_ARROW:
+
+      if (shapes[count].blocks) {
+        shapes[count].MoveDown()
+      }
+
+
+      break;
+
+    default:
+
+  }
+
   background(220)
+  shapes[count].Show()
+}
 
- 
+function LineLine(u1, v1, u2, v2, x1, y1, x2, y2) {
 
-  u1 = 500
-  u2 = mouseX
-
-  v1 = 400
-  v2 = mouseY
-
-
-  omega = abs((v1-y2) / (x2 - u1))
-  theta = abs((v1 -v2) / (u2 -u1))
-  a = abs((v1 - y1) / (x1 -u1))
-
-  line(u1, v1, u2, v2)
-  line(x1, y1, x2, y2)
-
-  
+  omega = abs((v1 - y2) / (x2 - u1))
+  theta = abs((v1 - v2) / (u2 - u1))
+  a = abs((v1 - y1) / (x1 - u1))
 
   m_free = (v2 - v1) / (u2 - u1)
   m_fix = (y2 - y1) / (x2 - x1)
@@ -48,61 +103,18 @@ function draw() {
 
 
 
-  dis = sqrt((v2-v1)**2 + (u2-u1)**2)
+  dis = sqrt((v2 - v1) ** 2 + (u2 - u1) ** 2)
 
-  min_dis = sqrt((Au - u1)**2 + (Bu - v1)**2)
-  // Au < x2 && Au > x1 && Bu > y1 && Bu < y2 &&
-  
-  // && theta > omega && theta < a
-  
-  if (dis > min_dis&& theta > min(omega,a) && theta < max(omega,a) )  {
+  min_dis = sqrt((Au - u1) ** 2 + (Bu - v1) ** 2)
 
-    fill(255, 255, 0)
-    circle(Au, Bu, 20)
+  if (dis > min_dis && theta > min(omega, a) && theta < max(omega, a)) {
+
+    return true;
   }
   else {
-    fill(255)
-    circle(Au, Bu, 20)
+
+    return false;
 
   }
 
-
 }
-
-
-function mouseClicked() {
-
-
-
-}
-
-
-
-function keyPressed() {
-
-  switch (keyCode) {
-    case LEFT_ARROW:
-
-      a.MoveLeft()
-      break;
-    case RIGHT_ARROW:
-
-      a.MoveRight()
-      break;
-    case 82:
-
-      a.Rotate()
-      break;
-
-    case DOWN_ARROW:
-
-      a.GetDown()
-      break;
-
-    default:
-
-  }
-
-  background(220)
-  a.Show()
-} 
